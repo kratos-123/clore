544dbbe5b782cf9692abb60727156e65d483e27e
Vv <bobmailvv@gmail.com>
2024-05-27 11:08:21 +0800

    fix

--- tests/nvidia.rs
+++ tests/nvidia.rs
@@ -11,14 +11,20 @@ mod test {
         crate::common::setup();
 
         let nvml = Nvml::init().unwrap();
+        info!("{:?}",nvml);
         // Get the first `Device` (GPU) in the system
         let device = nvml.device_by_index(0).unwrap();
-
+        info!("{:?}",device);
         let brand = device.brand().unwrap(); // GeForce on my system
+        info!("{:?}",brand);
         let fan_speed = device.fan_speed(0).unwrap(); // Currently 17% on my system
+        info!("{:?}",fan_speed);
         let power_limit = device.enforced_power_limit().unwrap(); // 275k milliwatts on my system
+        info!("{:?}",power_limit);
         let encoder_util = device.encoder_utilization().unwrap(); // Currently 0 on my system; Not encoding anything
+        info!("{:?}",encoder_util);
         let memory_info = device.memory_info().unwrap(); // Currently 1.63/6.37 GB used on my system
+        info!("{:?}",memory_info);
 
         // ... and there's a whole lot more you can do. Most everything in NVML is wrapped and ready to go
     }
