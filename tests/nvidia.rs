ceb567bedf127db7eb453a874352b3790b997a13
Vv <bobmailvv@gmail.com>
2024-05-27 12:28:38 +0800

    fix

--- tests/nvidia.rs
+++ tests/nvidia.rs
@@ -1,5 +1,3 @@
-
-
 pub mod common;
 #[cfg(test)]
 mod test {
@@ -12,30 +10,29 @@ mod test {
 
         let nvml = Nvml::init().unwrap();
 
-        let device_count = nvml.device_count().unwrap(); 
-        info!("device_count:{:?}",device_count);
+        let device_count = nvml.device_count().unwrap();
+        info!("device_count:{:?}", device_count);
 
-        info!("{:?}",nvml);
+        info!("{:?}", nvml);
         // Get the first `Device` (GPU) in the system
         let device = nvml.device_by_index(0).unwrap();
-        info!("{:?}",device);
+        info!("{:?}", device);
         let brand = device.brand().unwrap(); // GeForce on my system
-        info!("brand:{:?}",brand);
+        info!("brand:{:?}", brand);
         let fan_speed = device.fan_speed(0).unwrap(); // Currently 17% on my system
-        info!("fan_speed:{:?}",fan_speed);
+        info!("fan_speed:{:?}", fan_speed);
         let power_limit = device.enforced_power_limit().unwrap(); // 275k milliwatts on my system
-        info!("power_limit:{:?}",power_limit);
+        info!("power_limit:{:?}", power_limit);
         let encoder_util = device.encoder_utilization().unwrap(); // Currently 0 on my system; Not encoding anything
-        info!("encoder_util:{:?}",encoder_util);
+        info!("encoder_util:{:?}", encoder_util);
         let memory_info = device.memory_info().unwrap(); // Currently 1.63/6.37 GB used on my system
-        info!("memory_info:{:?}",memory_info);
+        info!("memory_info:{:?}", memory_info);
 
-        let running_compute_processes = device.running_compute_processes().unwrap(); 
-        info!("running_compute_processes:{:?}",running_compute_processes);
-
-
-
-        
-        // ... and there's a whole lot more you can do. Most everything in NVML is wrapped and ready to go
+        let running_compute_processes = device.running_compute_processes().unwrap();
+        info!("running_compute_processes:{:?}", running_compute_processes);
+        for process_info in running_compute_processes.iter() {
+            let sys_process_name = nvml.sys_process_name(process_info.pid, 150);
+            info!("sys_process_name:{:?}", sys_process_name);
+        }
     }
 }
