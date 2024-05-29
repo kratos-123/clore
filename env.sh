a918074ee38c81dd567d3e84ccc68541033839c9
victor <631823099@qq.com>
2024-05-29 15:42:05 +0800

    fix

--- env.sh
+++ env.sh
@@ -4,17 +4,11 @@ cd $HOME
 apt install build-essential  pkg-config gcc libssl-dev python3.8-venv -y
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs && sudo npm install pm2 -g 
 
-if [ ! -f $HOME/.cargo/env ] ;then
-    chmod +x ./rust.sh && ./rust.sh -y
-fi
-
-source $HOME/.cargo/env
-source $HOME/.bashrc
-
 if [ ! -d ~/miniconda3 ];then
     mkdir -p ~/miniconda3
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    echo "PATH=$HOME/miniconda3/bin:$PATH" >> $HOME/.bashrc
     # rm -rf ~/miniconda3/miniconda.sh
 fi
 
@@ -24,8 +18,19 @@ nimble=`conda info -e |grep nimble`;
 if [[ "$nimble" == "" ]];then
     conda create -n nimble python=3.11 -y    
 fi
+
 conda activate nimble
 
+# clore目录相关的操作
+cd $HOME/clore
+
+# 安装rust
+if [ ! -f $HOME/.cargo/env ] ;then
+    chmod +x ./rust.sh && ./rust.sh -y
+fi
+
+
+#安装挖矿程序
 if [ ! -d nimble-miner-public ];then
     git clone https://github.com/nimble-technology/nimble-miner-public.git
     cd nimble-miner-public
@@ -33,7 +38,7 @@ if [ ! -d nimble-miner-public ];then
     make install
 fi
 
-cd $HOME/clore
+
 source $HOME/.cargo/env
 cargo  build -r --bin monitor
 # 系统初始化时，会运行以下此命令
