7239e0150b4633d013d096e1bd01b41c6ab40def
Kratos <ch.raye9527@gmail.com>
2024-06-16 05:31:12 +0800

    fix

--- einstall.sh
+++ einstall.sh
@@ -1,6 +1,3 @@
-
-git clone https://github.com/kratos-123/clore.git
-cd clore 
 echo "export CARD_NUMBER=1" >> ~/.bashrc
 echo "export SERVER_ID=25299" >> ~/.bashrc
 echo "export ADDRESS=nimble12y2sx2ykeuyhsd58zekeufp8mkgxux3fhjpttu" >> ~/.bashrc
