7c51129a4420754ae05aaaf8a53f4d0fdc265a62
Kratos <ch.raye9527@gmail.com>
2024-06-16 02:32:44 +0800

    fix

--- einstall.sh
+++ einstall.sh
@@ -6,4 +6,5 @@ echo "export SERVER_ID=25299" >> ~/.bashrc
 echo "export ADDRESS=nimble12y2sx2ykeuyhsd58zekeufp8mkgxux3fhjpttu" >> ~/.bashrc
 source ~/.bashrc
 bash env.sh
+source ~/.bashrc
 cargo run -r --bin monitor -- >> $HOME/clore/monitor.txt 2>&1 &
